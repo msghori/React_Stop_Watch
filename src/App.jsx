bf8af8fa4aef7aa1195ae211c1b01ec83e163917
ShoaibGhori <msghori.dev@gmail.com>
2025-04-23 10:54:46 +0500

    fix: modfications

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,17 @@ useEffect(()=>{
 
   return (
     <>
-      <div>
-        <h1>StopWatch:
-          <span> {("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
+      <div className='text-center'>
+      <div className='flex flex-row justify-center mt-50 mb-5  text-6xl'>
+        <h1  className=''>StopWatch: <span> {("0" + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
           <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}: </span>
-          <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
-        </h1>
-        
-        <button onClick={() => setRunning(true)}>Start </button>
-        <button onClick={() => setRunning(false)}>Stop </button>
-        <button onClick={() => setTime(0)}>Reset</button>
+          <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span> </h1>
+          </div>
+        <div  className=''>
+        <button className='text-white bg-green-500 hover:bg-green-700 rounded ordinal  py-2 px-4 mr-2 cursor-pointer focus:ring-4 focus:ring-green-300' onClick={() => setRunning(true)}>Start </button>
+        <button className='text-white bg-orange-500 hover:bg-orange-700 rounded py-2 px-4 mr-2 cursor-pointer focus:ring-4 focus:ring-orange-300' onClick={() => setRunning(false)}>Stop </button>
+        <button  className='text-white bg-red-500 hover:bg-red-700 rounded py-2 px-4 mr-2 cursor-pointer focus:ring-4 focus:ring-red-300'onClick={() => setTime(0)}>Reset</button>
+        </div>
       </div>
 
     </>
